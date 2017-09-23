100adc97127d9287a2f51debb37f2d3db5ba48f9
Raghu Betina <raghu@firstdraft.com>
2017-09-23 20:36:23 +0000

    Direct association between movies and roles

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,10 @@
 class Movie < ApplicationRecord
   # Direct associations
 
+  has_many   :roles,
+             :foreign_key => "movies_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
